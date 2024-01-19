fee03a56da3bed4cece78ae4143555edcb62b640
No Nuts November <89920558+phuvo05@users.noreply.github.com>
2024-01-19 08:23:29 +0700

    Data_Images_Labels

--- FaceDetection.py
+++ FaceDetection.py
@@ -2,22 +2,29 @@ import os
 import time
 import uuid
 import cv2
+import tensorflow as tf
+import json
+import numpy as np 
+from matplotlib import pyplot as plt
 
-IMAGES_PATH = os.path.join('data', 'images') #Path of images
-number_images = 30 
 
-cap = cv2.VideoCapture(0)  
-for imgnum in range( number_images): 
-    print(f'Collecting image {imgnum}')
-    ret, frame = cap.read()
-    imgname = os.path.join(IMAGES_PATH,f'{str(uuid.uuid1())}.jpg')
-    cv2.imwrite(imgname, frame) #
-    cv2.imshow('frame', frame)  # Using cv2.imshow() method 
-                                # Displaying the image 
-    time.sleep(0.5)
-    
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-cap.release()
-cv2.destroyAllWindows() # closing all open windows 
+def get_data():
+    IMAGES_PATH = os.path.join('data', 'images') #Path of images
+    number_images = 30 
 
+    cap = cv2.VideoCapture(0)  
+    for imgnum in range( number_images): 
+        print(f'Collecting image {imgnum}')
+        ret, frame = cap.read()
+        imgname = os.path.join(IMAGES_PATH,f'{str(uuid.uuid1())}.jpg')
+        cv2.imwrite(imgname, frame) #
+        cv2.imshow('frame', frame)  # Using cv2.imshow() method 
+                                    # Displaying the image 
+        time.sleep(0.5)
+        
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    cap.release()
+    cv2.destroyAllWindows() # closing all open windows 
+
+def 
